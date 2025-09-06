e75b55bc0a630be3cddb3bcaf0a7d465a12a3b6e
Mubarek Shikur <manmuba90@gmail.com>
2025-09-06 16:24:01 +0300

    Done

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -119,7 +119,7 @@ export default function Home() {
 
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
 
-              products.map((product) => (
+              {products.map((product) => (
                 <ProductCard
                   key={product.id}
                   product={product}
